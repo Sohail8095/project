e339e6571ff1c65ff2079dc9275527dfde0028db
Ubuntu <ubuntu@ip-172-31-20-213.ap-southeast-1.compute.internal>
2018-12-25 06:51:09 +0000

    docker compose also installed

---
+++ dockercompose_one.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+version="1.18.0"
+
+#install docker compose for ubuntu for $version
+sudo curl -L https://github.com/docker/compose/releases/download/$version/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+
+#set the permissions
+sudo chmod +x /usr/local/bin/docker-compose
+
+#echo "$?"
+docker-compose --version
+if [ $? == 0 ]
+then
+        echo "installation is successfull"
+else
+        echo "not installed"
+fi
+
