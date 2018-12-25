73c3cfbcbd107e078e08b254fd30d841d876fd52
Sohail8095 <45159284+Sohail8095@users.noreply.github.com>
2018-12-25 12:57:11 +0530

    updated openssl

--- openssl_one.sh
+++ openssl_one.sh
@@ -3,23 +3,23 @@
 version="1.1.1"
 
 #install openssl for ubuntu, for $version
-wget https://www.openssl.org/source/openssl-$version.tar.gz
+wget https://www.openssl.org/source/openssl-$version.tar.gz > ~/autom/openssl.log
 tar xvf openssl-$version.tar.gz
 
 #to explore openssl
-cd openssl-$version
+cd openssl-$version >> ~/autom/openssl.log
 
 #to run the file
-sudo ./config -Wl,--enable-new-dtags,-rpath,'$(LIBRPATH)'
+sudo ./config -Wl,--enable-new-dtags,-rpath,'$(LIBRPATH)' >> ~/autom/openssl.log
 
 #to proceed
-sudo make
+sudo make >> ~/autom/openssl.log
 
 #finall to install
-sudo make install
+sudo make install >> ~/autom/openssl.log
 
 #installed openssl
-openssl --version
+openssl --version >> ~/autom/openssl.log
 if [ $? == 0 ]
 then
         echo "successfully installed"
